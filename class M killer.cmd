3624e778de9a771297a3c073329d55a49854d442
POWERHELL <47339652+BackSam6348@users.noreply.github.com>
2023-05-17 03:55:14 +0900

    Update class M killer.cmd

--- class M killer.cmd
+++ class M killer.cmd
@@ -1,13 +1,4 @@
-set file=.\hacker voice.mp3
-( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
-  echo Sound.URL = "%file%"
-  echo Sound.Controls.play
-  echo do while Sound.currentmedia.duration = 0
-  echo wscript.sleep 100
-  echo loop
-  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
-start /min sound.vbs
-set file=.\nuke.mp3
+set file=.\n1.mp3
 ( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
   echo Sound.URL = "%file%"
   echo Sound.Controls.play
