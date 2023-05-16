c5d25a6869909e9b575b03f62a47c55b2f4ea709
POWERHELL <47339652+BackSam6348@users.noreply.github.com>
2023-05-17 03:54:00 +0900

    Update class M killer V.2.23.bat

--- class M killer V.2.23.bat
+++ class M killer V.2.23.bat
@@ -4,11 +4,13 @@ color 2
 set /a a+=1
 color 5
 mode 1000
-echo Hacked Process!
+start sys1.vbs
+echo Starting Process killing...
 sc config ClassM_Client_Service start=demend
 sc stop ClassM_Client_Service
 NET STOP ClassM_Client_Service
 taskkill -F -T /PID SysCtrl.exe
 taskkill -F -T /PID mvnc.exe
 taskkill -F -T /PID ClassM_Client_Service.exe
+echo Process Killing Complete!
 goto loop
