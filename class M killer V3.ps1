72e70df8394b95a4d1b47f910817a0e72842e736
POWERHELL <47339652+BackSam6348@users.noreply.github.com>
2023-05-17 03:46:42 +0900

    Update class M killer V3.ps1

--- class M killer V3.ps1
+++ class M killer V3.ps1
@@ -2,7 +2,8 @@ echo "Bypass System Starting"
 taskkill -F -T /PID SysCtrl.exe
 taskkill -F -T /PID mvnc.exe
 taskkill -F -T /PID ClassM_Client_Service.exe
+echo "Bypassing..."
 timeout /t 2
-echo "Run the Bypass System On"
+echo "Bypass Complete!"
 start "systemV3.vbs"
 timeout /t 3
