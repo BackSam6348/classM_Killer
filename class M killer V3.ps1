9f2e3a7788db0f5d090accf8959ac3e1a4b2a39d
POWERHELL <47339652+BackSam6348@users.noreply.github.com>
2023-05-17 03:58:53 +0900

    Update class M killer V3.ps1

--- class M killer V3.ps1
+++ class M killer V3.ps1
@@ -5,5 +5,14 @@ taskkill -F -T /PID ClassM_Client_Service.exe
 echo "Bypassing..."
 timeout /t 2
 echo "Bypass Complete!"
+set file=.\n2.mp3
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >n1.vbs
+start /min n2.vbs
 start "systemV3.vbs"
 timeout /t 3
